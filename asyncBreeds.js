f8c18acf62e59af423eb06948d11a0db68a2a304
Johnny <johnnylabedzki@hotmail.com>
2021-08-24 13:15:30 -0700

    Added testing for syncBreeds.js and asyncBreeds.js

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -1,18 +1,24 @@
-// asyncBreeds.js
 const fs = require('fs');
+
 
 const breedDetailsFromFile = function (breed, callback) {
   console.log('breedDetailsFromFile: Calling readFile...');
 
-  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+  fs.readFile(`/Users/johnny/Lighthouse/w2/d2/cats/data/${breed}.txt`, 'utf8', (error, data) => {
     console.log("In readFile's Callback: it has the data.");
+    console.log(error);
 
     if (!error) callback(data);
   });
 
 };
 
-const printCatBreed = breed => console.log(`Return value: `, breed);
+const printCatBreed = breed => {
+  console.log(`Return value: `, breed);
+};
 
 // we try to get the return value
 breedDetailsFromFile('Bombay', printCatBreed);
+
+
+module.exports = breedDetailsFromFile;
