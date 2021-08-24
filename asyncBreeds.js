18c961396ae868d0fdff1a0daf29b3ab0ee82b02
Johnny <johnnylabedzki@hotmail.com>
2021-08-23 19:36:14 -0700

    Created a function to get a return value from an asynchronous function using a callback.

---
+++ asyncBreeds.js
@@ -0,0 +1,18 @@
+// asyncBreeds.js
+const fs = require('fs');
+
+const breedDetailsFromFile = function (breed, callback) {
+  console.log('breedDetailsFromFile: Calling readFile...');
+
+  fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
+    console.log("In readFile's Callback: it has the data.");
+
+    if (!error) callback(data);
+  });
+
+};
+
+const printCatBreed = breed => console.log(`Return value: `, breed);
+
+// we try to get the return value
+breedDetailsFromFile('Bombay', printCatBreed);
